fc9a2ade34124402e8ad9c3c4d9b156eaa912981
ncoresreality023 <reality023@n-cores.co.kr>
2022-11-14 01:18:27 +0900

    initialized

---
+++ lib/component/category_card.dart
@@ -0,0 +1,48 @@
+import 'package:dusty_dust/component/card_title.dart';
+import 'package:dusty_dust/component/main_card.dart';
+import 'package:dusty_dust/component/main_stat.dart';
+import 'package:dusty_dust/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class CategoryCard extends StatelessWidget {
+  const CategoryCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 160,
+      child: MainCard(
+        child: LayoutBuilder(
+          builder: (context, constraint) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const CardTitle(title: "종류별 통계"),
+                const SizedBox(height: 5),
+                Expanded(
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    physics: PageScrollPhysics(),
+                    children: List.generate(
+                      20,
+                      (index) => MainStat(
+                        width: constraint.maxWidth / 3,
+                        category: "미세먼지$index",
+                        imgPath: "asset/img/best.png",
+                        level: "최고",
+                        stat: "0㎍/㎥",
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 5),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:dusty_dust/component/category_card.dart';
+import 'package:dusty_dust/component/hourly_card.dart';
+import 'package:dusty_dust/component/main_app_bar.dart';
+import 'package:dusty_dust/component/main_drawer.dart';
+import 'package:dusty_dust/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primaryColor,
+      drawer: const MainDrawer(),
+      body: CustomScrollView(
+        slivers: [
+          const MainAppBar(),
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: const [
+                CategoryCard(),
+                SizedBox(height: 16.0),
+                HourlyCard(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
